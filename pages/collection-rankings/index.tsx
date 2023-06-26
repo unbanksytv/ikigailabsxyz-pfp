d701e92d58d723a36d7c65bdae12e6ebf074dd7e
UnbanksyTV <45092543+unbanksytv@users.noreply.github.com>
2023-06-26 05:53:20 +0200

    Update index.tsx

--- pages/collection-rankings/index.tsx
+++ pages/collection-rankings/index.tsx
@@ -126,7 +126,7 @@ const IndexPage: NextPage<Props> = ({ ssr }) => {
             }}
           >
             <Text style="h4" as="h4">
-              Top 33 PFP Collections By Volume
+              Top 42 PFP Collections By Volume
             </Text>
             <Flex align="center" css={{ gap: '$4' }}>
               <CollectionsTimeDropdown
